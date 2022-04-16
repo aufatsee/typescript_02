845c981f0cfe361757ceabfc71d078180911464b
aufatsee <aufatsee@gmail.com>
2022-04-16 23:46:34 +0800

    commit 8

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express, { Application, Request, Response } from "express";
 
+export * from "./node_file";
+export * from "./node_file_object";
+
+
 const app: Application = express();
 
 app.set("port", process.env.PORT || 3000);
